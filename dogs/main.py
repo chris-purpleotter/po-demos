6e84aaa8f4cb69e1985903913b15d00c60a3467f
chris-purpleotter <chris@purpleotter.ca>
2025-12-31 12:52:28 -0500

    Update main.py

--- dogs/main.py
+++ dogs/main.py
@@ -1,8 +1,11 @@
 import json
 import requests
 import os
+from google.cloud import storage
 url = os.environ.get('REQUEST_URL')
 endpoint = os.environ.get('REQUEST_ENDPOINT')
+storage_client = os.environ.get('STORAGE_CLIENT')
+storage_bucket = os.environ.get('STORAGE_BUCKET')
 
 def get_data(url,endpoint):
     try:
@@ -29,8 +32,18 @@ def transform_dogs():
             output = []
             for index,breed_name in enumerate(dog_breeds):
                 output.append({'breed':breed_name,'sub_breeds':data['message'][dog_breeds[index]]})
+            with open('transactions.json','w',encoding='utf-8') as f:
+                for i in output:
+                    json.dump(i,f)
+                    f.write('\n')
+                f.close()
+                file_path = 'automated_load'
+                file_name = 'transactions.json'
+                gcp_storage_client = storage.Client(storage_client)
+                gcp_storage_bucket = gcp_storage_client.bucket(storage_bucket)
+                file_upload = gcp_storage_bucket.blob(file_path + '/' + file_name)
+                file_upload.upload_from_filename(file_name)    
             print(output)
-            print('test')
         except:
             print("Error creating output.")
     except:
